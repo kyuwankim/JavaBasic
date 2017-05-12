0db62b222cd39110c64843430340ddc48bf958ae
kyuwankim <lpnids@gmail.com>
2017-05-12 11:15:18 +0900

    bbs 제목, 이름, 내용 입력

--- src/com/kyuwan/java/basic/BasicMain.java
+++ src/com/kyuwan/java/basic/BasicMain.java
@@ -7,21 +7,32 @@ import com.kyuwan.java.basic.bbs.Bbs;
 public class BasicMain {
 
 	public static void main(String[] args) {
-
+		Bbs bbs = new Bbs();
 		Scanner sc = new Scanner(System.in);
-		
-		while(true){
+
 		String words = sc.nextLine();
 		System.out.println(words);
-		}
-		
-		
-//		Bbs bbs = new Bbs();
-//		bbs.title = "안녕하세요";
-//		bbs.author = "홍길동";
-//		bbs.date = System.currentTimeMillis();
-//		bbs.content = "content";
+
+		System.out.println("제목을 입력 하세요 : ");
+		bbs.title = sc.nextLine();
+
+		System.out.println("작성자를 입력 하세요 : ");
+		bbs.author = sc.nextLine();
+
+		System.out.println("내용을 입력 하세요 : ");
+		bbs.content = sc.nextLine();
+
+		System.out.println("----------------------");
+		System.out.println("제목 : "+bbs.title);
+		System.out.println("작성자 : "+bbs.author);
+		System.out.println("내용 : "+bbs.content);
+	
+
+		// Bbs bbs = new Bbs();
+		// bbs.title = "안녕하세요";
+		// bbs.author = "홍길동";
+		// bbs.date = System.currentTimeMillis();
+		// bbs.content = "content";
 	}
 
 }
-
