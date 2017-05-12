72fb121ffe685c304c2665627e86c5c33af710ac
kyuwankim <lpnids@gmail.com>
2017-05-12 11:03:47 +0900

    Scanner 생성

--- src/com/kyuwan/java/basic/BasicMain.java
+++ src/com/kyuwan/java/basic/BasicMain.java
@@ -1,16 +1,26 @@
 package com.kyuwan.java.basic;
 
+import java.util.Scanner;
+
 import com.kyuwan.java.basic.bbs.Bbs;
 
 public class BasicMain {
 
 	public static void main(String[] args) {
 
-		Bbs bbs = new Bbs();
-		bbs.title = "¾È³çÇÏ¼¼¿ä";
-		bbs.author = "È«±æµ¿";
-		bbs.date = System.currentTimeMillis();
-		bbs.content = "content";
+		Scanner sc = new Scanner(System.in);
+		
+		String words = sc.nextLine();
+		
+		System.out.println(words);
+		
+		
+		
+//		Bbs bbs = new Bbs();
+//		bbs.title = "¾È³çÇÏ¼¼¿ä";
+//		bbs.author = "È«±æµ¿";
+//		bbs.date = System.currentTimeMillis();
+//		bbs.content = "content";
 	}
 
 }
