473be9133c25cde83e51667dda8a3d6ec7bcfcb1
kyuwankim <lpnids@gmail.com>
2017-05-12 11:22:35 +0900

    Array -> ArrayList 변경 및 향상된 for 문을 통한 출력 추가

--- src/com/kyuwan/java/basic/BasicMain.java
+++ src/com/kyuwan/java/basic/BasicMain.java
@@ -1,5 +1,6 @@
 package com.kyuwan.java.basic;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.kyuwan.java.basic.bbs.Bbs;
@@ -8,28 +9,36 @@ public class BasicMain {
 
 	public static void main(String[] args) {
 		Bbs bbs = new Bbs();
-		Bbs datas[] = new Bbs[100];
-		
+		ArrayList<Bbs> datas = new ArrayList<>();
+
 		Scanner sc = new Scanner(System.in);
 
 		String words = sc.nextLine();
 		System.out.println(words);
 
-		System.out.println("제목을 입력 하세요 : ");
-		bbs.title = sc.nextLine();
+		for (int i = 0; i < 3; i++) {
+			System.out.println("제목을 입력 하세요 : ");
+			bbs.title = sc.nextLine();
 
-		System.out.println("작성자를 입력 하세요 : ");
-		bbs.author = sc.nextLine();
+			System.out.println("작성자를 입력 하세요 : ");
+			bbs.author = sc.nextLine();
 
-		System.out.println("내용을 입력 하세요 : ");
-		bbs.content = sc.nextLine();
+			System.out.println("내용을 입력 하세요 : ");
+			bbs.content = sc.nextLine();
 
-		System.out.println("----------------------");
-		System.out.println("제목 : "+bbs.title);
-		System.out.println("작성자 : "+bbs.author);
-		System.out.println("내용 : "+bbs.content);
-	
-		datas[0] = bbs;
+			System.out.println("----------------------");
+			System.out.println("제목 : " + bbs.title);
+			System.out.println("작성자 : " + bbs.author);
+			System.out.println("내용 : " + bbs.content);
+
+			datas.add(bbs);
+		}
+		
+		for( Bbs item : datas){
+			System.out.print(item.title);
+			System.out.print(" ");
+			System.out.println(item.date);
+		}
 
 		// Bbs bbs = new Bbs();
 		// bbs.title = "안녕하세요";
