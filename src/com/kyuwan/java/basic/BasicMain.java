77cb98c870a62d6c6e863d88cd19a2a949d74670
kyuwankim <lpnids@gmail.com>
2017-05-12 10:41:41 +0900

    Java 변수 설명

--- src/com/kyuwan/java/basic/BasicMain.java
+++ src/com/kyuwan/java/basic/BasicMain.java
@@ -3,7 +3,19 @@ public class BasicMain {
 
 	public static void main(String[] args) {
 
+	String className = "안드로이드 스쿨";
+	int classSize = 20;
+	int studentCount = 15;
 	
+	String student1Name = "홍길동";
+	int student1Age = 15;
+	
+	String student2Name = "이순신";
+	int student2Age = 33;
+	
+	String student3Name = "세종대왕";
+	int student3Age = 27;
+
 	}
 
 }
