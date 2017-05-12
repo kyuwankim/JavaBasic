00dd8451404151b10f4b2b625a7ac694ee215449
kyuwankim <lpnids@gmail.com>
2017-05-12 10:45:00 +0900

    Class 생성

--- src/com/kyuwan/java/basic/BasicMain.java
+++ src/com/kyuwan/java/basic/BasicMain.java
@@ -1,22 +1,28 @@
 package com.kyuwan.java.basic;
+
 public class BasicMain {
 
 	public static void main(String[] args) {
 
-	String className = "안드로이드 스쿨";
-	int classSize = 20;
-	int studentCount = 15;
-	
-	String student1Name = "홍길동";
-	int student1Age = 15;
-	
-	String student2Name = "이순신";
-	int student2Age = 33;
-	
-	String student3Name = "세종대왕";
-	int student3Age = 27;
+		String className = "안드로이드 스쿨";
+		int classSize = 20;
+		int studentCount = 15;
+
+		String student1Name = "홍길동";
+		int student1Age = 15;
+
+		String student2Name = "이순신";
+		int student2Age = 33;
+
+		String student3Name = "세종대왕";
+		int student3Age = 27;
 
 	}
 
 }
-	
+
+class Class {
+	String name;
+	int size;
+	int studentCount;
+}
